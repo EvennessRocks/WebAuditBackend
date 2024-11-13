3c21930692f15b7d19510185c2d79f9fcf4a427c
Alan Subedi <alansubedi@Alans-Laptop.local>
2024-11-12 22:34:45 -0600

    added fixes for potential cors issues

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const auditRoutes = require('./routes/auditRoutes');
 
 const app = express();
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+}));
+
 app.use(express.json());
 
 // Set up routes
