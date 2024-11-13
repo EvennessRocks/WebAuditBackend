e867cbe8797c8ce01fa6bcb224ed805a8b7069db
Alan Subedi <alansubedi@Alans-Laptop.local>
2024-11-12 20:28:09 -0600

    fix puppeter prod issue

--- utils/axeAudit.js
+++ utils/axeAudit.js
@@ -2,11 +2,18 @@ const puppeteer = require('puppeteer');
 const axeCore = require('axe-core');
 
 exports.runAxeAudit = async (url) => {
-  const browser = await puppeteer.launch();
+  // Launch Puppeteer with additional flags for production environments
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'], // Add these flags
+    headless: true, // Ensure the browser runs in headless mode (no GUI)
+  });
+
   const page = await browser.newPage();
 
+  // Navigate to the specified URL
   await page.goto(url);
 
+  // Inject axe-core and run it
   await page.evaluate(axeSource => eval(axeSource), axeCore.source);
   const results = await page.evaluate(() => axe.run());
 
